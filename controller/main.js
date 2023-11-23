24e95e5ac2e94194f748b8d647d5ac882f21db56
Trần Ngọc Dương <lotaynaongo@gmail.com>
2023-11-24 03:13:27 +0700

    apply check authentication to all screens

--- controller/main.js
+++ controller/main.js
@@ -1,17 +1,3 @@
-// LOGIN
-var loginBtn = document.getElementById('login-btn');
-var logoutBtn = document.getElementById('logout-btn');
-
-if (localStorage.getItem('isLoggedIn') === 'true') {
-    loginBtn.style.display = 'none';
-} else {
-    logoutBtn.style.display = 'none';
-}
-
-function logout() {
-    // Xóa trạng thái đăng nhập từ Local Storage
-    localStorage.removeItem('isLoggedIn');
-}
 // SLIDER
 let list = document.querySelector('.slider .list-slide');
 let slides = document.querySelectorAll('.slider .list-slide .slide');
